29e4f15c3a8c4e292edb616de6a14ee9d2a2682a
AjinkyaMukkirwar11 <126233753+AjinkyaMukkirwar11@users.noreply.github.com>
2024-06-19 09:25:36 +0000

    Date in Details

---
+++ 01_Basic/08_Datesinjs.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+// let myCurrentDate = new Date(2023, 0, 23)
+let myCurrentDate = new Date(2023, 0, 23)
+// let myCurrentDate = new Date(2023, 0, 23)
+console.log(myCurrentDate.toDateString());
