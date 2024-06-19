22922a483de79f48508a6b70d831fdf12dff4bc4
AjinkyaMukkirwar11 <126233753+AjinkyaMukkirwar11@users.noreply.github.com>
2024-06-19 08:18:40 +0000

    more on string

--- 01_Basic/06_strings.js
+++ 01_Basic/06_strings.js
@@ -11,3 +11,21 @@ console.log(gameName.__proto__);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(1));
 console.log(gameName.indexOf(`A`));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-12,4)
+console.log(anotherString);
+
+
+const newStringOne = "   ajinkya  "
+console.log(newStringOne.trim());
+
+const url = "https://ajinkya.com/ajinkya%20mukkirwar"
+
+console.log(url.replace(`%20`, `-`));
+
+console.log(url.includes(`nidhi`)); 
+
+console.log(gameName.split(`-`));
