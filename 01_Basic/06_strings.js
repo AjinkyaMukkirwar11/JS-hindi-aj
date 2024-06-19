46636a5b93542612875bd0333a37e7085813c354
AjinkyaMukkirwar11 <126233753+AjinkyaMukkirwar11@users.noreply.github.com>
2024-06-19 04:36:46 +0000

    string more

--- 01_Basic/06_strings.js
+++ 01_Basic/06_strings.js
@@ -1,4 +1,13 @@
-const name = "Ajinkya"
+const name = "Ajinkya "
 const repoCount = 50
 
-console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String (`Ajinkya`)
+console.log(gameName);
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(1));
+console.log(gameName.indexOf(`A`));
