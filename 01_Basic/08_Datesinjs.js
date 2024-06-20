433191f4f07a2e06e3dab67ea925f4307d86b3b3
AjinkyaMukkirwar11 <126233753+AjinkyaMukkirwar11@users.noreply.github.com>
2024-06-20 04:21:04 +0000

    new indate

--- 01_Basic/08_Datesinjs.js
+++ 01_Basic/08_Datesinjs.js
@@ -9,6 +9,25 @@ let myDate = new Date()
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 9, 5)
-let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+let myCreatedDate = new Date("12-01-2023")
 // let myCreatedDate = new Date(2023, 0, 23)
-console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+
+
+console.log(newDate.toLocaleString(`default`, {
+    weekday: "long",
+}));
