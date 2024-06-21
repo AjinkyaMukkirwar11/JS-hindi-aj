a8687bbe9ca3d7771f34838695e7ef9e6347f572
AjinkyaMukkirwar11 <126233753+AjinkyaMukkirwar11@users.noreply.github.com>
2024-06-21 05:22:04 +0000

    more on array

--- 01_Basic/09_arraysinjs.js
+++ 01_Basic/09_arraysinjs.js
@@ -1,5 +1,46 @@
 // const aj = [1,2,3,4]
 // console.log(aj);
 
-const myArr = [0, 1, 2, 3, 4, 5]
+
+// Array 
+ const myArr = [0, 1, 2, 3, 4, 5]
+// const myHeros = ["shaktiman", "nagraj"]
+// console.log(myArr[1]); 
+
+ const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myHeros.length);
+// console.log(myHeros.length);
+// console.log(myArr2);
+
+// array methods
   
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift() 
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+// slice Splice 
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("c ", myArr);
+console.log(myn2);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+// console.log(myArr);
