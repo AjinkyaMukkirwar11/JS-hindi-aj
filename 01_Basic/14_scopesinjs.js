9d720fd237703001b19f07ced3300f88c3aed29a
AjinkyaMukkirwar11 <126233753+AjinkyaMukkirwar11@users.noreply.github.com>
2024-06-24 11:05:19 +0000

    more on scoe n hosting

--- 01_Basic/14_scopesinjs.js
+++ 01_Basic/14_scopesinjs.js
@@ -7,11 +7,54 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+  //  console.log("INNER: ", a);
 }
 
 
 
-console.log(a); 
+//console.log(a); 
 //console.log(b); 
 // console.log(c); 
+
+function one() {
+    const username = "Ajinkya"
+
+    function two() {
+        const website = "youtube"
+       // console.log(username);
+    }
+    //console.log(website);
+
+   // two()
+}
+// console.log(username);
+one()
+
+
+
+if (true) {
+    const username = "ajinkya"
+    if (username === "ajinkya") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+// ********************************* interesting **********************************
+
+
+console.log(addone(5))
+function addone(num) {
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num) {
+    return num
+}
