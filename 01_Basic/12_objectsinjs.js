e680256367dde5023b6ea542b35d85cde68efe12
AjinkyaMukkirwar11 <126233753+AjinkyaMukkirwar11@users.noreply.github.com>
2024-06-24 09:25:42 +0000

    de structure in object

--- 01_Basic/12_objectsinjs.js
+++ 01_Basic/12_objectsinjs.js
@@ -46,11 +46,40 @@ const users = [
 
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
-console.log(tinderUser.hasOwnProperty(`isLogged`));
+// console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
+// console.log(tinderUser.hasOwnProperty(`isLogged`));
+
+// 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Ajinkya"
+}
+
+// course.courseInstructor
+
+const {courseInstructor} = course  // destructuring in object
+
+console.log(courseInstructor);
+
+
+// {
+//     "name": "Ajinkya",
+//     "coursename": "js in hindi",
+//     "price": "free"
+
+// }
+
+
+// [ 
+//     {},
+//     {},
+//     {}
+// ]
