f1b60d420ee2d5f92220265ea3d1d74ff8a12aa6
AjinkyaMukkirwar11 <126233753+AjinkyaMukkirwar11@users.noreply.github.com>
2024-06-24 10:32:16 +0000

    scope detais

---
+++ 01_Basic/14_scopesinjs.js
@@ -0,0 +1,17 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+let a = 300
+// if scope is a block scope
+if(true){
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+}
+
+
+
+console.log(a); 
+//console.log(b); 
+// console.log(c); 
