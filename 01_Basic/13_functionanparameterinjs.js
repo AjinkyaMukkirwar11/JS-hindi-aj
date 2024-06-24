7d9af8814c076070dfe668874771e4eba7ce237d
AjinkyaMukkirwar11 <126233753+AjinkyaMukkirwar11@users.noreply.github.com>
2024-06-24 10:18:34 +0000

    more on function

--- 01_Basic/13_functionanparameterinjs.js
+++ 01_Basic/13_functionanparameterinjs.js
@@ -45,4 +45,35 @@ function loginUserMessage(username = "Aj") {
     return `${username} just logged in`
 }
 
-console.log (loginUserMessage("Mukkirwar"))
+// console.log (loginUserMessage("Mukkirwar"))
+
+// ...num is a rest operator in function
+function claculateCardPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(claculateCardPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Ajinkya",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user) 
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100 , 600]));
